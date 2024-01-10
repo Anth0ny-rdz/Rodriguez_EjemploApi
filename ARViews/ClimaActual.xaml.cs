be11fe9d42699465040ea7a7f87f0b08b3b998bf
Anthony Josué Rodríguez Camuendo <anthony.rodriguez.camuendo@udla.edu.ec>
2024-01-10 08:57:15 -0500

    cambios

--- ARViews/ClimaActual.xaml.cs
+++ ARViews/ClimaActual.xaml.cs
@@ -1,3 +1,8 @@
+using Rodriguez_EjemploApi.Model;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+
 namespace Rodriguez_EjemploApi.ARViews;
 
 public partial class ClimaActual : ContentPage
@@ -6,4 +11,29 @@ public partial class ClimaActual : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		string latitud = Lat.Text;
+		string longitud = lon.Text;
+
+		if(Connectivity.NetworkAccess==NetworkAccess.Internet)
+		{
+			using (var client  = new HttpClient()) 
+			
+			{
+				string url = $"https://api.openweathermap.org/data/2.5/weather?Lat=" + latitud + "&lon=" + longitud + "&appid= ";
+					var response = await client.GetAsync(url);
+				if(response.IsSuccessStatusCode)
+				
+				{
+				var json= await response.Content.ReadAsStringAsync();
+					var clima = JsonConvert.DeserializeObject<Rootobject>(json);
+
+					weatherLabel.Text = clima.weather[0].main;
+				}
+			}
+
+		}
+    }
 }
